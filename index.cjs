c0f5af08a4744acd4c7dd8856eadfb73a5ba3694
dodovibesofficial-debug <dodovibesofficial@gmail.com>
2026-01-27 22:49:26 +0100

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -6520,7 +6520,7 @@ client.on(Events.MessageCreate, async (message) => {
             new EmbedBuilder()
               .setColor(COLOR_RED)
               .setDescription(
-                "• `❗`  __Masowy ping jest niedozwolony otrzymujesz mute na 1 godzine__",
+                "• `❗`  **__Masowy ping jest niedozwolony otrzymujesz mute na 1 godzine__**",
               ),
           ],
         });
